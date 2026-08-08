2905bd767fb72f7454fdf42a95c83a0169cf7b02
ArthurKova <metaloutcome@gmail.com>
2026-08-08 12:04:07 +0300

    lesson 1

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const users = require('./users')
-const { getCurrentYear } = require('./date')
-const currentMonth = require('./date').getCurrentMonth()
+const users = require("./users");
+const { getCurrentYear } = require("./date");
+const currentMonth = require("./date").getCurrentMonth();
 
-const {admins} = users
-const currentYear = getCurrentYear()
+const { admins } = users;
 
+const currentYear = getCurrentYear();
 
-
-console.log('Hello World with Nodemon', admins, {
-    currentYear,
-  currentMonth
-})
+console.log("Hello World with Nodemon", admins, {
+	currentYear,
+	currentMonth,
+});
